7fa1360a0aba97ebcf7efe5a23b79a72dc0c32e3
扈文韬 <vernon_hu@163.com>
2015-08-11 17:58:48 +0800

    增加百度地图

--- tutor/src/main/java/www/ht/com/app/ui/TutorApplication.java
+++ tutor/src/main/java/www/ht/com/app/ui/TutorApplication.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.Application;
 import android.os.Bundle;
 
+import com.baidu.mapapi.SDKInitializer;
 import com.orhanobut.logger.LogLevel;
 import com.orhanobut.logger.Logger;
 
@@ -28,6 +29,8 @@ public class TutorApplication extends Application {
         dbUtils = DbUtils.create(this);
 
         registerActivityLifecycleCallbacks(new HTActivityLifecycleCallbacks());
+
+        SDKInitializer.initialize(getApplicationContext());//因此我们建议该方法放在Application的初始化方法中
     }
 
     public DbUtils getDbUtils() {
--- tutor/src/main/java/www/ht/com/app/ui/fragment/MapFragment.java
+++ tutor/src/main/java/www/ht/com/app/ui/fragment/MapFragment.java
@@ -7,6 +7,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.baidu.mapapi.map.MapView;
+
 import www.ht.com.app.R;
 import www.ht.com.app.ui.BaseFragment;
 
@@ -18,6 +20,7 @@ public class MapFragment extends BaseFragment {
 
     private static final String ARG_TITLE = "ARG_TITLE";
     private String mParamTitle;
+    private MapView mMapView;
 
     public static MapFragment newInstance(String Title) {
         MapFragment fragment = new MapFragment();
@@ -47,5 +50,31 @@ public class MapFragment extends BaseFragment {
         return inflater.inflate(R.layout.fragment_map, container, false);
     }
 
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //获取地图控件引用
+        mMapView = (MapView) view.findViewById(R.id.bmapView);
+    }
 
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
+        mMapView.onDestroy();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
+        mMapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
+        mMapView.onPause();
+    }
 }
